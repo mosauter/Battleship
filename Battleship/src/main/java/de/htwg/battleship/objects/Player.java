528fe0bb62132c1675731433c90b09b4186834b2
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-11-26 18:53:23 +0100

    Changed to save only the own board of the player

--- Battleship/src/main/java/de/htwg/battleship/objects/Player.java
+++ Battleship/src/main/java/de/htwg/battleship/objects/Player.java
@@ -9,15 +9,28 @@ package de.htwg.battleship.objects;
  */
 public class Player {
 
+    /**
+     * Saves the board of the specified Player.
+     */
     private final Board ownBoard;
-    private final Board shootBoard;
+    /**
+     * Name of the Player.
+     */
     private String name = "";
 
-    public Player(Board player1Board, Board player2Board) {
+    /**
+     * Public Constructor.
+     * @param player1Board Board of the Player.
+     */
+    public Player(final Board player1Board) {
         ownBoard = player1Board;
-        shootBoard = player2Board;
     }
 
+    /**
+     * Setter for the name.
+     * Only works one time.
+     * @param name String name of the player
+     */
     public final void setName(final String name) {
         if (this.name.isEmpty()) {
             this.name = name;
@@ -32,11 +45,11 @@ public class Player {
         return this.name;
     }
 
+    /**
+     * Getter for the Board of the Player.
+     * @return Board
+     */
     public final Board getOwnBoard() {
         return ownBoard;
-    }
-
-    public final Board getShootBoard() {
-        return shootBoard;
     }
 }
