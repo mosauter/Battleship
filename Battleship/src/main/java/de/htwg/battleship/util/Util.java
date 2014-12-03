1176dfa7c3fea5be040a41b51a1acf1b3057b910
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-03 18:47:41 +0100

    added a Utility Collection

---
+++ Battleship/src/main/java/de/htwg/battleship/util/Util.java
@@ -0,0 +1,24 @@
+// Util.java
+
+package de.htwg.battleship.util;
+
+/**
+ * Utility Collection of static methods.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-03
+ */
+public class Util {
+
+    /**
+     * Utility-Method to check if a value is between.
+     * @param xupp upper border
+     * @param xlow lower border
+     * @param x value
+     * @return true if the value is between
+     */
+    public static boolean isBetween(final int xupp,
+            final int xlow, final int x) {
+        return (x >= xlow && x <= xupp);
+    }
+}
