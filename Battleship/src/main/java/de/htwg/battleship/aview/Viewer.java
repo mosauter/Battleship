f1dce031d6190110b04c1b63ac5e086f80430309
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-09 18:40:38 +0100

    created Viewer Interface, should implement a StatePattern

---
+++ Battleship/src/main/java/de/htwg/battleship/aview/Viewer.java
@@ -0,0 +1,14 @@
+//Viewer.java
+
+package de.htwg.battleship.aview;
+
+/**
+ * Viewer is an Utility-Interface.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00 
+ * @since 2014-12-09
+ */
+public interface Viewer {
+
+    public String getString();
+}
