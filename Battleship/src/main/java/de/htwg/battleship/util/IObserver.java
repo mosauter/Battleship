3c75db620ce8bc687980056b2275f673b53f3bd0
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-10 22:31:17 +0100

    Interface for the Observer

---
+++ Battleship/src/main/java/de/htwg/battleship/util/IObserver.java
@@ -0,0 +1,17 @@
+//IObserver.java
+
+package de.htwg.battleship.util;
+
+/**
+ * IObserver is an Utility-Interface.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-10
+ */
+public interface IObserver {
+
+    /**
+     * Method to Update the IObserver.
+     */
+    void update();
+}
