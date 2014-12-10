d0be3ed22b4e41eea9f3722b04ae177fa12a3eb0
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-10 18:06:17 +0100

    added Interface for Chain

---
+++ Battleship/src/main/java/de/htwg/battleship/controller/ICollisionController.java
@@ -0,0 +1,30 @@
+//ICollisionControler.java
+
+package de.htwg.battleship.controller;
+
+import de.htwg.battleship.model.Ship;
+
+/**
+ * ICollisionControler is an Utility-Interface.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-10
+ */
+public interface ICollisionController {
+
+    /**
+     * Method which checks if the ship collides.
+     * @param shipIn first ship
+     * @param ship second ship
+     * @return true if collides
+     */
+    boolean isCollision(Ship shipIn, Ship ship);
+
+    /**
+     * Checks if the implementation is responsible for the case.
+     * @param shipIn first ship
+     * @param ship second ship
+     * @return true if they collide
+     */
+    boolean responsibility(Ship shipIn, Ship ship);
+}
