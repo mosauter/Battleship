6bfce6ee38a3f5c6f118385ee4896c394cd09db1
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-10 23:13:51 +0100

    only created a TUI

---
+++ Battleship/src/main/java/de/htwg/battleship/aview/TUI.java
@@ -0,0 +1,13 @@
+// TUI.java
+
+package de.htwg.battleship.aview;
+
+/**
+ * TUI
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-10
+ */
+public class TUI {
+
+}
