c758612fad73719b562ccb590edca3f09bbea144
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-10 17:59:16 +0100

    added some comments and now print board of both player

--- Battleship/src/main/java/de/htwg/battleship/aview/FieldViewer.java
+++ Battleship/src/main/java/de/htwg/battleship/aview/FieldViewer.java
@@ -4,39 +4,72 @@ package de.htwg.battleship.aview;
 
 import de.htwg.battleship.model.Board;
 import de.htwg.battleship.model.Player;
+import static de.htwg.battleship.util.StatCollection.HEIGTH_LENGTH;
 
 /**
- * FieldViewer
+ * FieldViewer to print the Field.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-09
  */
 public class FieldViewer implements Viewer {
-    private Player player1;
-    private Player player2;
-    
+
+    /**
+     * Saves Player one.
+     */
+    private final Player player1;
+    /**
+     * Saves Player two.
+     */
+    private final Player player2;
+
+    /**
+     * Public - Constructor.
+     * @param player1 player one
+     * @param player2 player two
+     */
     public FieldViewer(final Player player1, final Player player2) {
         this.player1 = player1;
-        this.player1 = player2;
+        this.player2 = player2;
     }
 
-    public String getString() {
+    /**
+     * Method to get the String-Presentation of the Field.
+     * @return String
+     */
+    public final String getString() {
         StringBuilder sb = new StringBuilder();
         sb.append(" ");
         Board boardPlayer1 = player1.getOwnBoard();
-        for (int i = 0; i < 10; i++) {
+        Board boardPlayer2 = player2.getOwnBoard();
+        for (int i = 0; i < HEIGTH_LENGTH; i++) {
             char c = (char) ('a' + i);
             sb.append(" ").append(c);
         }
-        sb.append("\n1");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        sb.append("\t  ");
+        for (int i = 0; i < HEIGTH_LENGTH; i++) {
+            char c = (char) ('a' + i);
+            sb.append(" ").append(c);
+        }
+        sb.append("\n");
+        for (int i = 0; i < HEIGTH_LENGTH; i++) {
+            sb.append(i);
+            for (int j = 0; j < HEIGTH_LENGTH; j++) {
                 if (boardPlayer1.getField(j, i).isHit()) {
                     sb.append(" X");
                 } else {
                     sb.append(" O");
                 }
             }
+            sb.append("\t ").append(i);
+            for (int j = 0; j < HEIGTH_LENGTH; j++) {
+                if (boardPlayer2.getField(j, i).isHit()) {
+                    sb.append(" X");
+                } else {
+                    sb.append(" O");
+                }
+            }
+            sb.append("\n");
         }
         return sb.toString();
     }
