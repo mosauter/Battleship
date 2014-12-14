d23871e2eb91cebd262b3fa8deccd11f2d56dfcd
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-14 14:07:43 +0100

    added comments

--- Battleship/src/main/java/de/htwg/battleship/controller/impl/DestroyedController.java
+++ Battleship/src/main/java/de/htwg/battleship/controller/impl/DestroyedController.java
@@ -7,19 +7,42 @@ import de.htwg.battleship.model.IPlayer;
 import de.htwg.battleship.model.IShip;
 
 /**
- * DestroyedController
+ * DestroyedController to check if a player is completely destroyed.
+ * implements Chain-of-Responsibility
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-11
  */
 public abstract class DestroyedController {
 
+    /**
+     * Saves the orientation of the ship.
+     * used to check if the implementation is responsible for the specific case
+     */
     boolean ship;
+    /**
+     * Reference to the next implementation of the controller.
+     */
     DestroyedController next;
-    
+
+    /**
+     * Method to check if a ship is destroyed of a player.
+     * @param ship specified ship
+     * @param player specified player, owner of the ship
+     * @return true if the ship is completely destroyed
+     *         false if not
+     */
     protected abstract boolean isDestroyed(IShip ship, IPlayer player);
-    
-    public boolean responsibility(IShip ship, IPlayer player) {
+
+    /**
+     * Method to check if the implementation is responsible for the case.
+     * @param ship specified ship
+     * @param player specified player, owner of the ship
+     * @return true if the ship is completely destroyed
+     *         false if not
+     */
+    public final boolean responsibility(final IShip ship,
+            final IPlayer player) {
         if (ship.isOrientation() == this.ship) {
             return isDestroyed(ship, player);
         }
@@ -27,15 +50,24 @@ public abstract class DestroyedController {
     }
 }
 
+/**
+ * Implementation for the Destroyed Controller.
+ * responsible for the case the ships orientation is true
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ */
 class DestroyedTrueController extends DestroyedController {
 
+    /**
+     * Public constructor.
+     * implements also the next controller in the chain
+     */
     public DestroyedTrueController() {
         this.ship = true;
         this.next = new DestroyedFalseController();
     }
 
     @Override
-    protected boolean isDestroyed(IShip ship, IPlayer player) {
+    protected boolean isDestroyed(final IShip ship, final IPlayer player) {
         int xlow = ship.getX();
         int xupp = xlow + ship.getSize();
         int y = ship.getY();
@@ -49,14 +81,23 @@ class DestroyedTrueController extends DestroyedController {
     }
 }
 
+/**
+ * Implementation for the Destroyed Controller.
+ * responsible for the case the ships orientation is false
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ */
 class DestroyedFalseController extends DestroyedController {
 
+    /**
+     * Public Constructor.
+     * last controller in the chain
+     */
     public DestroyedFalseController() {
         this.ship = false;
     }
 
     @Override
-    protected boolean isDestroyed(IShip ship, IPlayer player) {
+    protected boolean isDestroyed(final IShip ship, final IPlayer player) {
         int ylow = ship.getY();
         int yupp = ylow + ship.getSize();
         int x = ship.getX();
