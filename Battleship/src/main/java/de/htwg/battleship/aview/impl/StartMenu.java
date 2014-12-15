f6f412f8fa4ef088bdcd9fb2a2be2a6a039ff7c3
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-15 17:29:23 +0100

    added tab

--- Battleship/src/main/java/de/htwg/battleship/aview/impl/StartMenu.java
+++ Battleship/src/main/java/de/htwg/battleship/aview/impl/StartMenu.java
@@ -16,7 +16,7 @@ public class StartMenu implements Viewer {
         StringBuilder sb = new StringBuilder();
         sb.append("1. Start Game\n");
         sb.append("2. Exit\n");
-        sb.append("\t--> ");
+        sb.append("\t--->\t");
         return sb.toString();
     }
 
