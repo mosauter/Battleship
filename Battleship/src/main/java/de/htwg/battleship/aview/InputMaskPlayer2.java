149f206eac4c94a4c8d96d25d92c05223242489e
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-15 17:27:29 +0100

    Created another viewer

---
+++ Battleship/src/main/java/de/htwg/battleship/aview/InputMaskPlayer2.java
@@ -0,0 +1,17 @@
+// InputMaskPlayer2.java
+
+package de.htwg.battleship.aview;
+
+/**
+ * InputMaskPlayer2
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-15
+ */
+public class InputMaskPlayer2 implements Viewer {
+
+    public String getString() {
+        return "\nName Player 2:\t";
+    }
+
+}
