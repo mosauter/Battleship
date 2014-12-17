2da85615e319d0c458ac8517da69c0cf7561d340
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-17 19:10:14 +0100

    created Viewer for the Place error

---
+++ Battleship/src/main/java/de/htwg/battleship/controller/impl/PlaceErrorViewer.java
@@ -0,0 +1,20 @@
+// PlaceErrorViewer.java
+
+package de.htwg.battleship.controller.impl;
+
+import de.htwg.battleship.controller.Viewer;
+
+/**
+ * PlaceErrorViewer presents a error-Message.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-17
+ */
+public class PlaceErrorViewer implements Viewer {
+
+    @Override
+    public final String getString() {
+        return "Cannot place the Ship because there was a collision "
+                + "or the ship ends out of the field. Try again!\n";
+    }
+}
