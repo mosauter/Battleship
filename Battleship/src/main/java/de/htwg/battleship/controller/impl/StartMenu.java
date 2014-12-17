0f8c501c50c3b683bea372d489cddc830a67c3a0
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-17 19:37:59 +0100

    added getter for the START state

--- Battleship/src/main/java/de/htwg/battleship/controller/impl/StartMenu.java
+++ Battleship/src/main/java/de/htwg/battleship/controller/impl/StartMenu.java
@@ -3,6 +3,8 @@
 package de.htwg.battleship.controller.impl;
 
 import de.htwg.battleship.controller.Viewer;
+import de.htwg.battleship.util.State;
+import static de.htwg.battleship.util.State.START;
 
 /**
  * StartMenu presents a start Menu for the game.
@@ -19,5 +21,10 @@ public class StartMenu implements Viewer {
         sb.append("2. Exit\n");
         sb.append("\t--->\t");
         return sb.toString();
+    }
+
+    @Override
+    public final State getCurrentState() {
+        return START;
     }
 }
