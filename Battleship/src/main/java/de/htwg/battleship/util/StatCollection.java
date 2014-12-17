dba9cc6bdd964d4387936b7fb92ed154f2bca2b5
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-17 20:28:13 +0100

    added more utility methods which help to create a field presentation

--- Battleship/src/main/java/de/htwg/battleship/util/StatCollection.java
+++ Battleship/src/main/java/de/htwg/battleship/util/StatCollection.java
@@ -5,6 +5,8 @@ package de.htwg.battleship.util;
 import de.htwg.battleship.model.IShip;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Set of static util Methods.
@@ -88,5 +90,46 @@ public final class StatCollection {
             }
             return map;
         }
+    }
+
+    /**
+     * Method to initialize the map for all the ships.
+     * @return Map<Integer, Set<Integer>> empty with
+     *         HEIGHT_LENGTH of empty sets
+     */
+    public static Map<Integer, Set<Integer>> createMap() {
+        TreeMap<Integer, Set<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < HEIGTH_LENGTH; i++) {
+            map.put(i, new TreeSet<Integer>());
+        }
+        return map;
+    }
+
+    /**
+     * Method to create the legend of both fields.
+     * @param sb StringBuilder on which the method has to add a legend
+     * @return the sb mentioned above with a legend
+     */
+    public static StringBuilder createBorder(final StringBuilder sb) {
+        for (int i = 0; i < HEIGTH_LENGTH; i++) {
+            char c = (char) ('a' + i);
+            sb.append(" ").append(c);
+        }
+        return sb;
+    }
+
+    /**
+     * Method to fill a Map with ships.
+     * @param shipList specified ships
+     * @param map Map where to save the ships
+     * @param ships how much ships are in the list
+     * @return the new Map
+     */
+    public static Map<Integer, Set<Integer>> fillMap(final IShip[] shipList,
+            final Map<Integer, Set<Integer>> map, final int ships) {
+        for (int i = 0; i < ships; i++) {
+            getSet(shipList[i], map);
+        }
+        return map;
     }
 }
