9a04a0ba0e055421e590b5287a79e2a6c34d0a7d
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-17 19:37:04 +0100

    added method to get the WIN 1 / 2 state

--- Battleship/src/main/java/de/htwg/battleship/controller/impl/WinPlayer.java
+++ Battleship/src/main/java/de/htwg/battleship/controller/impl/WinPlayer.java
@@ -2,8 +2,12 @@
 
 package de.htwg.battleship.controller.impl;
 
+import de.htwg.battleship.controller.IMasterController;
 import de.htwg.battleship.controller.Viewer;
 import de.htwg.battleship.model.IPlayer;
+import de.htwg.battleship.util.State;
+import static de.htwg.battleship.util.State.WIN1;
+import static de.htwg.battleship.util.State.WIN2;
 
 /**
  * WinPlayer1 presents a view which player has won.
@@ -17,13 +21,20 @@ public class WinPlayer implements Viewer {
      * Saves the winner.
      */
     private final IPlayer player;
+    /**
+     * True if the winner is the first player,
+     * false if the winner is the second player.
+     */
+    private final boolean firstPlayer;
 
     /**
      * Public Constructor.
      * @param player the winner
+     * @param master the master controller
      */
-    public WinPlayer(final IPlayer player) {
+    public WinPlayer(final IPlayer player, final IMasterController master) {
         this.player = player;
+        firstPlayer = this.player.equals(master.getPlayer1());
     }
 
     @Override
@@ -32,5 +43,13 @@ public class WinPlayer implements Viewer {
         sb.append(player.getName());
         sb.append(" has won!\n\n");
         return sb.toString();
+    }
+
+    @Override
+    public final State getCurrentState() {
+        if (firstPlayer) {
+            return WIN1;
+        }
+        return WIN2;
     }
 }
