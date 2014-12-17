7cc3052ea48ca76c2e40b423f09d3546724aa94b
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-17 19:56:23 +0100

    added return GETNAME1 state

--- Battleship/src/main/java/de/htwg/battleship/controller/impl/InputMaskPlayer1.java
+++ Battleship/src/main/java/de/htwg/battleship/controller/impl/InputMaskPlayer1.java
@@ -3,6 +3,8 @@
 package de.htwg.battleship.controller.impl;
 
 import de.htwg.battleship.controller.Viewer;
+import de.htwg.battleship.util.State;
+import static de.htwg.battleship.util.State.GETNAME1;
 
 /**
  * InputMask presents the input mask for player one.
@@ -15,5 +17,10 @@ public class InputMaskPlayer1 implements Viewer {
     @Override
     public final String getString() {
         return "\nName Player 1:\t";
+    }
+
+    @Override
+    public State getCurrentState() {
+        return GETNAME1;
     }
 }
