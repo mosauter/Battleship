cf7a38cfde9b7bf0431ef27835589eb3d232e8a3
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-18 13:33:05 +0100

    changed to the new eventbased tui

--- Battleship/src/main/java/de/htwg/battleship/Battleship.java
+++ Battleship/src/main/java/de/htwg/battleship/Battleship.java
@@ -6,6 +6,7 @@ import de.htwg.battleship.controller.IMasterController;
 import de.htwg.battleship.controller.impl.MasterController;
 import de.htwg.battleship.model.IPlayer;
 import de.htwg.battleship.model.impl.Player;
+import java.util.Scanner;
 /**
  * Battleship start file.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
@@ -30,18 +31,24 @@ public final class Battleship {
      * Saves Player Two.
      */
     private static IPlayer player2;
+    /**
+     * Scanner to read from stdin.
+     */
+    private static Scanner scanner;
 
     /**
      * Main-Method.
      * @param args not used
      */
     public static void main(final String[] args) {
-//        GUI gui = new GUI();
         player1 = new Player();
         player2 = new Player();
         master = new MasterController(player1, player2);
         TUI tui = new TUI(master);
-        tui.initialize();
-        tui.startGame();
+//        GUI gui = new GUI(master);
+        scanner = new Scanner(System.in);
+        while (true) {
+            tui.processInputLine(scanner.nextLine());
+        }
     }
 }
