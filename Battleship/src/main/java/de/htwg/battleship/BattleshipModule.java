d067227c933f43d8a9e0fdc5acba2dd60e907f5d
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-19 14:10:31 +0100

    began dependency injector

---
+++ Battleship/src/main/java/de/htwg/battleship/BattleshipModule.java
@@ -0,0 +1,35 @@
+// BattleshipModule.java
+
+package de.htwg.battleship;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import de.htwg.battleship.controller.IMasterController;
+import de.htwg.battleship.controller.impl.MasterController;
+import de.htwg.battleship.model.IBoard;
+import de.htwg.battleship.model.IField;
+import de.htwg.battleship.model.IPlayer;
+import de.htwg.battleship.model.IShip;
+import de.htwg.battleship.model.impl.Board;
+import de.htwg.battleship.model.impl.Field;
+import de.htwg.battleship.model.impl.Player;
+import de.htwg.battleship.model.impl.Ship;
+
+/**
+ * BattleshipModule
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-19
+ */
+public class BattleshipModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(IMasterController.class).to(
+                MasterController.class).in(Singleton.class);
+        bind(IBoard.class).to(Board.class);
+        bind(IShip.class).to(Ship.class);
+        bind(IField.class).to(Field.class);
+        bind(IPlayer.class).to(Player.class);
+    }
+}
