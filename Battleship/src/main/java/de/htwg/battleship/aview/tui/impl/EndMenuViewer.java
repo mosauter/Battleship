77bcbdac2de1c6b6263a4bc6201641ad533f1f96
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-29 16:54:15 +0100

    Menu for the new END-state and TUI

---
+++ Battleship/src/main/java/de/htwg/battleship/aview/tui/impl/EndMenuViewer.java
@@ -0,0 +1,23 @@
+// EndMenuViewer.java
+
+package de.htwg.battleship.aview.tui.impl;
+
+import de.htwg.battleship.aview.tui.Viewer;
+
+/**
+ * EndMenuViewer provides a menu after a game has won by a player.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-29
+ */
+public class EndMenuViewer implements Viewer {
+
+    @Override
+    public final String getString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("1. Start a new Game\n");
+        sb.append("2. End Game\n\n");
+        sb.append("\t\t-->\t");
+        return sb.toString();
+    }
+}
