d81211f4c365bca48fb2bdffef9096e2828c74eb
Moritz Sauter <mosauter@htwg-konstanz.de>
2014-12-30 23:59:39 +0100

    added explanation for the exit typing in the start-menu

--- Battleship/src/main/java/de/htwg/battleship/aview/tui/impl/StartMenu.java
+++ Battleship/src/main/java/de/htwg/battleship/aview/tui/impl/StartMenu.java
@@ -17,6 +17,7 @@ public class StartMenu implements Viewer {
         StringBuilder sb = new StringBuilder();
         sb.append("1. Start Game\n");
         sb.append("2. Exit\n");
+        sb.append("Type 'Exit' or 'exit' at any time to close the game.\n");
         sb.append("\t--->\t");
         return sb.toString();
     }
