7e9db9b08f4ece58ac20ca693d81efe7056cb0cd
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-02 23:56:34 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/controller/ShootControllerTest.java
+++ Battleship/src/test/java/de/htwg/battleship/controller/ShootControllerTest.java
@@ -7,28 +7,36 @@ import de.htwg.battleship.model.impl.Board;
 import de.htwg.battleship.model.impl.Player;
 import de.htwg.battleship.model.impl.Ship;
 import de.htwg.battleship.util.StatCollection;
-import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * ShootControllerTest
+ * ShootControllerTest tests the entire shoot controller.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-11-19
  */
 public class ShootControllerTest {
 
-    ShootController sc;
-    Player player1;
-    Player player2;
-    
-    public ShootControllerTest() {
-    }
+    /**
+     * Saves a shootController.
+     */
+    private ShootController sc;
+    /**
+     * Saves first player.
+     */
+    private Player player1;
+    /**
+     * Saves second player.
+     */
+    private Player player2;
 
+    /**
+     * Set-Up method.
+     */
     @Before
-    public void setUp() {
+    public final void setUp() {
         StatCollection.HEIGTH_LENGTH = 10;
         StatCollection.SHIP_NUMBER_MAX = 5;
         player1 = new Player(new Board());
@@ -40,8 +48,19 @@ public class ShootControllerTest {
         sc = new ShootController(player1, player2);
     }
 
-    @After
-    public void tearDown() {
+    /**
+     * Test shoot-Method.
+     * Hit-/Non-Hit - Test.
+     * Ship-Orientation is true.
+     */
+    @Test
+    public final void shootTestOneOriTrue() {
+        boolean expResult = true;
+        boolean result = sc.shoot(3, 2, false);
+        assertEquals(expResult, result);
+        expResult = false;
+        result = sc.shoot(3, 3, false);
+        assertEquals(expResult, result);
     }
 
     /**
@@ -50,22 +69,7 @@ public class ShootControllerTest {
      * Ship-Orientation is true.
      */
     @Test
-    public void shootTestOneOriTrue() {
-        boolean expResult = true;
-        boolean result = sc.shoot(3, 2, false);
-        assertEquals(expResult, result);
-        expResult = false;
-        result = sc.shoot(3, 3, false);
-        assertEquals(expResult, result);
-    }
-    
-    /**
-     * Test shoot-Method.
-     * Hit-/Non-Hit - Test.
-     * Ship-Orientation is true.
-     */
-    @Test
-    public void shootTestTwoOriTrue() {
+    public final void shootTestTwoOriTrue() {
         boolean expResult = false;
         boolean result = sc.shoot(3, 2, true); // first Time true, second time false
         result = sc.shoot(3, 2, true);
@@ -75,9 +79,12 @@ public class ShootControllerTest {
         result = sc.shoot(2, 2, true);
         assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test for shoot orientation false.
+     */
     @Test
-    public void shootTestOneOriFalse() {
+    public final void shootTestOneOriFalse() {
         boolean expResult = true;
         boolean result = sc.shoot(1, 1, true);
         assertEquals(expResult, result);
@@ -85,9 +92,12 @@ public class ShootControllerTest {
         result = sc.shoot(1, 1, true);
         assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test for shoot orientation false.
+     */
     @Test
-    public void shootTestTwoOriFalse() {
+    public final void shootTestTwoOriFalse() {
         boolean expResult = false;
         boolean result = sc.shoot(1, 0, false);
         assertEquals(expResult, result);
