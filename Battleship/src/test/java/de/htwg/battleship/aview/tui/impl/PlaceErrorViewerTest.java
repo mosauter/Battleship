0a019dd0833b3ce55d21b07c65838e0a85570bd3
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-02 23:47:52 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/PlaceErrorViewerTest.java
+++ Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/PlaceErrorViewerTest.java
@@ -3,34 +3,21 @@
 package de.htwg.battleship.aview.tui.impl;
 
 import de.htwg.battleship.aview.tui.Viewer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * PlaceErrorViewerTest
+ * PlaceErrorViewerTest tests the placeerror viewer.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class PlaceErrorViewerTest {
 
-    public PlaceErrorViewerTest() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of getString method, of class PlaceErrorViewer.
      */
     @Test
-    public void testGetString() {
+    public final void testGetString() {
         Viewer v = new PlaceErrorViewer();
         String s = "Cannot place the Ship because there was a collision or"
                 + " the ship ends out of the field. Try again!\n";
