80989046c68ba51aa6dcd1732960a9a389c0beac
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-03 00:09:40 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/controller/impl/CollisionOrientationFirstTrueTest.java
+++ Battleship/src/test/java/de/htwg/battleship/controller/impl/CollisionOrientationFirstTrueTest.java
@@ -2,31 +2,43 @@
 
 package de.htwg.battleship.controller.impl;
 
+import de.htwg.battleship.model.IShip;
 import de.htwg.battleship.model.impl.Ship;
 import de.htwg.battleship.util.StatCollection;
-import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * CollisionOrientationFirstTrueTest
+ * CollisionOrientationFirstTrueTest tests an implementation of the chain.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-04
  */
 public class CollisionOrientationFirstTrueTest {
 
-    CollisionOrientationFirstTrue cc;
-    Ship ship1;
-    Ship ship2;
-    Ship ship3;
+    /**
+     * Saves the implementation.
+     */
+    private CollisionOrientationFirstTrue cc;
+    /**
+     * Saves the first ship.
+     */
+    private IShip ship1;
+    /**
+     * Saves the second ship.
+     */
+    private IShip ship2;
+    /**
+     * Saves the third ship.
+     */
+    private IShip ship3;
 
-    public CollisionOrientationFirstTrueTest() {
-    }
-
+    /**
+     * Set-Up.
+     */
     @Before
-    public void setUp() {
+    public final void setUp() {
         StatCollection.HEIGTH_LENGTH = 10;
         StatCollection.SHIP_NUMBER_MAX = 5;
         cc = new CollisionOrientationFirstTrue();
@@ -35,15 +47,11 @@ public class CollisionOrientationFirstTrueTest {
         ship3 = new Ship(3, false, 5, 4);
     }
 
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of isCollision method, of class CollisionOrientationFirstTrue.
      */
     @Test
-    public void testIsCollision() {
+    public final void testIsCollision() {
         boolean expRes = false;
         boolean result = cc.isCollision(ship1, ship2);
         assertEquals(expRes, result);
@@ -51,5 +59,4 @@ public class CollisionOrientationFirstTrueTest {
         result = cc.isCollision(ship1, ship3);
         assertEquals(expRes, result);
     }
-
 }
