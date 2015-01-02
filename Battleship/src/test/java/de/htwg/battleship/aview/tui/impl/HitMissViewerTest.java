8b9cefa283ed6e98b8672a1c04e7922e8f30a676
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-02 23:45:20 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/HitMissViewerTest.java
+++ Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/HitMissViewerTest.java
@@ -3,41 +3,26 @@
 package de.htwg.battleship.aview.tui.impl;
 
 import de.htwg.battleship.aview.tui.Viewer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * HitMissViewerTest
+ * HitMissViewerTest tests the HitMissViewer.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class HitMissViewerTest {
 
-    Viewer v;
-    public HitMissViewerTest() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of getString method, of class HitMissViewer.
      */
     @Test
-    public void testGetString() {
-        v = new HitMissViewer(true);
+    public final void testGetString() {
+        Viewer v = new HitMissViewer(true);
         String s = "Your Shot was a Hit!!\n";
         assert (s.equals(v.getString()));
         v = new HitMissViewer(false);
         s = "Your Shot was a Miss\n";
         assert (s.equals(v.getString()));
     }
-
 }
