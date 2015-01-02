968fdb0c47124191604d49470c3f3ab6152ffb98
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-02 23:48:49 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/WrongInputViewerTest.java
+++ Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/WrongInputViewerTest.java
@@ -3,39 +3,24 @@
 package de.htwg.battleship.aview.tui.impl;
 
 import de.htwg.battleship.aview.tui.Viewer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * WrongInputViewerTest
+ * WrongInputViewerTest tests the wrong input viewer.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class WrongInputViewerTest {
 
-    Viewer view;
-    public WrongInputViewerTest() {
-    }
-
-    @Before
-    public void setUp() {
-        view = new WrongInputViewer();
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of getString method, of class WrongInputViewer.
      */
     @Test
-    public void testGetString() {
+    public final void testGetString() {
+        Viewer view = new WrongInputViewer();
         String s = "Your Input is not recognized and was false "
                 + "at the current state of the Game!!\n";
         assert (s.equals(view.getString()));
     }
-
 }
