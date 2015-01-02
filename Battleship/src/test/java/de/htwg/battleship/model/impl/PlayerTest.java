548d293528f472c43c128c0af22bd2d26933cef1
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-03 00:28:05 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/model/impl/PlayerTest.java
+++ Battleship/src/test/java/de/htwg/battleship/model/impl/PlayerTest.java
@@ -3,38 +3,41 @@ package de.htwg.battleship.model.impl;
 // PlayerTest.java
 
 import de.htwg.battleship.model.IBoard;
+import de.htwg.battleship.model.IPlayer;
 import de.htwg.battleship.util.StatCollection;
 import static de.htwg.battleship.util.StatCollection.HEIGTH_LENGTH;
-import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * PlayerTest
+ * PlayerTest tests the player implementation.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-11-06
  */
 public class PlayerTest {
 
-    Player player;
-    public PlayerTest() {
-    }
+    /**
+     * Saves player.
+     */
+    private IPlayer player;
 
+    /**
+     * Set-Up.
+     */
     @Before
-    public void setUp() {
+    public final void testSetUp() {
         StatCollection.HEIGTH_LENGTH = 10;
         StatCollection.SHIP_NUMBER_MAX = 5;
         player = new Player();
     }
 
-    @After
-    public void tearDown() {
-    }
-
+    /**
+     * Test for getOwnBoard method.
+     */
     @Test
-    public void getOwnBoard() {
+    public final void testGetOwnBoard() {
         Field[][] expResult = new Field[HEIGTH_LENGTH][HEIGTH_LENGTH];
         for (int x = 0; x < HEIGTH_LENGTH; x++) {
             for (int y = 0; y < HEIGTH_LENGTH; y++) {
@@ -48,9 +51,12 @@ public class PlayerTest {
             }
         }
     }
-    
+
+    /**
+     * Test for setName and getName methods.
+     */
     @Test
-    public void setGetName() {
+    public final void setGetName() {
         String expResult = "";
         String result = player.getName();
         assertEquals(expResult, result);
