8a83a86787816f886136a6493df605333a26c63d
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-03 00:32:40 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/util/StatesTest.java
+++ Battleship/src/test/java/de/htwg/battleship/util/StatesTest.java
@@ -3,37 +3,28 @@
 package de.htwg.battleship.util;
 
 import static de.htwg.battleship.util.State.HIT;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * StatesTest
+ * StatesTest tests the states.
+ * simple enum
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class StatesTest {
 
-    State state;
-    public StatesTest() {
-    }
-
-    @Before
-    public void setUp() {
-        
-    }
-
-    @After
-    public void tearDown() {
-    }
+    /**
+     * Saves a state.
+     */
+    private State state;
 
     /**
      * Test of values method, of class State.
      */
     @Test
-    public void testValues() {
+    public final void testValues() {
         state = State.valueOf("HIT");
         Assert.assertEquals(state, HIT);
     }
