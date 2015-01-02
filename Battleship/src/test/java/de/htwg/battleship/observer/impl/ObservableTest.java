936a18b44410c809281c054aba94e9d61bd5a154
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-03 00:31:28 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/observer/impl/ObservableTest.java
+++ Battleship/src/test/java/de/htwg/battleship/observer/impl/ObservableTest.java
@@ -3,56 +3,49 @@
 package de.htwg.battleship.observer.impl;
 
 import de.htwg.battleship.observer.IObserver;
-import org.junit.After;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * ObservableTest
+ * ObservableTest tests the observable.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class ObservableTest {
 
-    public ObservableTest() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of notifyObserver method, of class Observable.
      */
     @Test
-    public void testObserver() {
+    public final void testObserver() {
         ObservableTestObject oto = new ObservableTestObject();
         ObserverObject oo = new ObserverObject();
         oto.addObserver(oo);
-        assertEquals(1, oo.getTestInt());
+        assertEquals(1, oo.testInt);
         oto.notifyObserver();
-        assertEquals(0, oo.getTestInt());
+        assertEquals(0, oo.testInt);
     }
 
+    /**
+     * Test utility object.
+     */
     class ObservableTestObject extends Observable {
     }
-    
+
+    /**
+     * Test utility object.
+     */
     class ObserverObject implements IObserver {
 
+        /**
+         * Test int.
+         */
         private int testInt = 1;
+
         @Override
         public void update() {
             testInt = 0;
-        }
-
-        public int getTestInt() {
-            return testInt;
         }
     }
 }
