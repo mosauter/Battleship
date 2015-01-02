5db1ab12599171e321abbaf8f64f7ce28cf42948
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-02 23:46:26 +0100

    changed comments

--- Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/InputMaskPlayer1Test.java
+++ Battleship/src/test/java/de/htwg/battleship/aview/tui/impl/InputMaskPlayer1Test.java
@@ -3,38 +3,23 @@
 package de.htwg.battleship.aview.tui.impl;
 
 import de.htwg.battleship.aview.tui.Viewer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * InputMaskPlayer1Test
+ * InputMaskPlayer1Test tests the InputMask for player 1.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
  */
 public class InputMaskPlayer1Test {
 
-    Viewer v;
-    public InputMaskPlayer1Test() {
-    }
-
-    @Before
-    public void setUp() {
-        v = new InputMaskPlayer1();
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of getString method, of class InputMaskPlayer1.
      */
     @Test
-    public void testGetString() {
+    public final void testGetString() {
+        Viewer v = new InputMaskPlayer1();
         String s = "\nName Player 1:\t";
         assert (s.equals(v.getString()));
     }
-
 }
