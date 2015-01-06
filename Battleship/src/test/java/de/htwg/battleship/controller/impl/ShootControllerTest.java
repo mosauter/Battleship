d36491c8f9118c89fbe6217b920b1252f82b0e78
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-06 19:13:42 +0100

    moved in the right package

---
+++ Battleship/src/test/java/de/htwg/battleship/controller/impl/ShootControllerTest.java
@@ -0,0 +1,109 @@
+// ShootControllerTest.java
+
+package de.htwg.battleship.controller.impl;
+
+import de.htwg.battleship.controller.impl.ShootController;
+import de.htwg.battleship.model.impl.Board;
+import de.htwg.battleship.model.impl.Player;
+import de.htwg.battleship.model.impl.Ship;
+import de.htwg.battleship.util.StatCollection;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ShootControllerTest tests the entire shoot controller.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-11-19
+ */
+public class ShootControllerTest {
+
+    /**
+     * Saves a shootController.
+     */
+    private ShootController sc;
+    /**
+     * Saves first player.
+     */
+    private Player player1;
+    /**
+     * Saves second player.
+     */
+    private Player player2;
+
+    /**
+     * Set-Up method.
+     */
+    @Before
+    public final void setUp() {
+        StatCollection.HEIGTH_LENGTH = 10;
+        StatCollection.SHIP_NUMBER_MAX = 5;
+        player1 = new Player(new Board());
+        player2 = new Player(new Board());
+        Ship ship1 = new Ship(1, true, 3, 2 );
+        Ship ship2 = new Ship(1, false, 1, 1);
+        player1.getOwnBoard().addShip(ship1);
+        player2.getOwnBoard().addShip(ship2);
+        sc = new ShootController(player1, player2);
+    }
+
+    /**
+     * Test shoot-Method.
+     * Hit-/Non-Hit - Test.
+     * Ship-Orientation is true.
+     */
+    @Test
+    public final void shootTestOneOriTrue() {
+        boolean expResult = true;
+        boolean result = sc.shoot(3, 2, false);
+        assertEquals(expResult, result);
+        expResult = false;
+        result = sc.shoot(3, 3, false);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test shoot-Method.
+     * Hit-/Non-Hit - Test.
+     * Ship-Orientation is true.
+     */
+    @Test
+    public final void shootTestTwoOriTrue() {
+        boolean expResult = false;
+        boolean result = sc.shoot(3, 2, true); // first Time true, second time false
+        result = sc.shoot(3, 2, true);
+        assertEquals(expResult, result);
+        result = sc.shoot(4, 2, true);
+        assertEquals(expResult, result);
+        result = sc.shoot(2, 2, true);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test for shoot orientation false.
+     */
+    @Test
+    public final void shootTestOneOriFalse() {
+        boolean expResult = true;
+        boolean result = sc.shoot(1, 1, true);
+        assertEquals(expResult, result);
+        expResult = false;
+        result = sc.shoot(1, 1, true);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test for shoot orientation false.
+     */
+    @Test
+    public final void shootTestTwoOriFalse() {
+        boolean expResult = false;
+        boolean result = sc.shoot(1, 0, false);
+        assertEquals(expResult, result);
+        result = sc.shoot(1, 2, false);
+        assertEquals(expResult, result);
+        result = sc.shoot(0, 1, false);
+        assertEquals(expResult, result);
+    }
+}
---
+++ Battleship/src/test/java/de/htwg/battleship/controller/impl/WinControllerTest.java
@@ -0,0 +1,77 @@
+// WinControllerTest.java
+
+package de.htwg.battleship.controller.impl;
+
+import de.htwg.battleship.controller.impl.WinController;
+import de.htwg.battleship.model.IPlayer;
+import de.htwg.battleship.model.IShip;
+import de.htwg.battleship.model.impl.Player;
+import de.htwg.battleship.model.impl.Ship;
+import de.htwg.battleship.util.StatCollection;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * WinControllerTest tests the entire win controller.
+ * @author Moritz Sauter (SauterMoritz@gmx.de)
+ * @version 1.00
+ * @since 2014-12-14
+ */
+public class WinControllerTest {
+
+    /**
+     * Set-Up method.
+     */
+    @Before
+    public final void setUp() {
+        StatCollection.HEIGTH_LENGTH = 2;
+        StatCollection.SHIP_NUMBER_MAX = 1;
+    }
+
+    /**
+     * Test of win method, of class WinController.
+     */
+    @Test
+    public final void testNullWin() {
+        IPlayer player1 = new Player();
+        IPlayer player2 = new Player();
+        IShip ship = new Ship(1, true, 0, 0);
+        player1.getOwnBoard().addShip(ship);
+        player2.getOwnBoard().addShip(ship);
+        WinController wc = new WinController(player1, player2);
+        IPlayer expRes = null;
+        IPlayer result = wc.win();
+        assertEquals(expRes, result);
+    }
+
+    /**
+     * Test of win method, result player1.
+     */
+    @Test
+    public final void testPlayer1Win() {
+        IPlayer player1 = new Player();
+        IPlayer player2 = new Player();
+        IShip ship = new Ship(1, true, 0, 0);
+        player1.getOwnBoard().addShip(ship);
+        WinController wc = new WinController(player1, player2);
+        IPlayer expRes = player1;
+        IPlayer result = wc.win();
+        assertEquals(expRes, result);
+    }
+
+    /**
+     * Test of win method, result player2.
+     */
+    @Test
+    public final void testPlayer2Win() {
+        IPlayer player1 = new Player();
+        IPlayer player2 = new Player();
+        IShip ship = new Ship(1, true, 0, 0);
+        player2.getOwnBoard().addShip(ship);
+        WinController wc = new WinController(player1, player2);
+        IPlayer expRes = player2;
+        IPlayer result = wc.win();
+        assertEquals(expRes, result);
+    }
+}
