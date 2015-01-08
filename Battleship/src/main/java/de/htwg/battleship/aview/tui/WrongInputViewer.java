479f80ee7b70f3c0ef521f4ea7b6fd7e8e07d4bf
root <root@Moritz-UX31A>
2015-01-08 18:56:58 +0100

    changed comments

--- Battleship/src/main/java/de/htwg/battleship/aview/tui/WrongInputViewer.java
+++ Battleship/src/main/java/de/htwg/battleship/aview/tui/WrongInputViewer.java
@@ -3,7 +3,7 @@
 package de.htwg.battleship.aview.tui;
 
 /**
- * WrongInputViewer
+ * WrongInputViewer is a presentation as a result of a wrong input.
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-18
@@ -11,7 +11,7 @@ package de.htwg.battleship.aview.tui;
 public class WrongInputViewer implements Viewer {
 
     @Override
-    public String getString() {
+    public final String getString() {
         return "Your Input is not recognized and was false"
                 + " at the current state of the Game!!\n";
     }
