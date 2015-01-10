57ad5e395cacc3e57aa4e449893d62631facfc73
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-01-10 01:08:44 +0100

    pushed to full branch coverage

--- Battleship/src/test/java/de/htwg/battleship/controller/impl/CollisionOrientationBothFalseTest.java
+++ Battleship/src/test/java/de/htwg/battleship/controller/impl/CollisionOrientationBothFalseTest.java
@@ -5,6 +5,7 @@ package de.htwg.battleship.controller.impl;
 import de.htwg.battleship.model.IShip;
 import de.htwg.battleship.model.impl.Ship;
 import de.htwg.battleship.util.StatCollection;
+import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -58,5 +59,11 @@ public class CollisionOrientationBothFalseTest {
         expRes = true;
         result = cc.isCollision(ship1, ship3);
         assertEquals(expRes, result);
+    }
+
+    @Test
+    public final void testIsCollisionFalseUnder() {
+        Ship sh = new Ship(1, false, -1, 1);
+        Assert.assertFalse(cc.isCollision(ship1, sh));
     }
 }
