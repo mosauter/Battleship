f83e9e661ba8ca9574207bf348e62abe2b022497
Moritz Sauter <SauterMoritz@gmx.de>
2015-01-23 13:18:43 +0100

    began to work at different GameModes

---
+++ Battleship/src/main/java/de/htwg/battleship/util/GameMode.java
@@ -0,0 +1,19 @@
+package de.htwg.battleship.util;
+
+/**
+ * GameMode is an enum for the different modes of the Game.
+ * @author Moritz Sauter <SauterMoritz@gmx.de>
+ * @since 2015-01-23
+ * @version 1.00
+ */
+public enum GameMode {
+	/**
+	 * Mode to indicate the usual GameMode.
+	 */
+	NORMAL,
+	/**
+	 * Mode to indicate that wich player 
+	 * has to shot always changes after one round.
+	 */
+	EXTREME;
+}
