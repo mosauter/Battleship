b0844e187e7efbafb962122129bd62a19a145661
Moritz Sauter <SauterMoritz@gmx.de>
2015-04-02 18:17:52 +0200

    changed the reset a little

--- Battleship/src/main/java/de/htwg/battleship/model/IPlayer.java
+++ Battleship/src/main/java/de/htwg/battleship/model/IPlayer.java
@@ -1,9 +1,10 @@
-//IPlayer.java
+// IPlayer.java
 
 package de.htwg.battleship.model;
 
 /**
  * IPlayer is an Utility-Interface.
+ * 
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-10
@@ -13,21 +14,28 @@ public interface IPlayer {
     /**
      * Setter for the name.
      * Only works one time.
-     * @param name String name of the player
+     * 
+     * @param name
+     *            String name of the player
      */
     void setName(String name);
+
     /**
      * Getter for the Name of the Player.
+     * 
      * @return String
      */
     String getName();
+
     /**
      * Getter for the Board of the Player.
+     * 
      * @return Board
      */
     IBoard getOwnBoard();
+
     /**
      * Method resets the board of the player.
      */
-    void resetBoard();
+    void resetBoard(IBoard board);
 }
