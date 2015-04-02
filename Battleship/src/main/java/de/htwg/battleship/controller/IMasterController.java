b58d49f4b064d164166aa2e5f7853db8ef5d2246
Moritz Sauter <SauterMoritz@gmx.de>
2015-04-02 18:15:50 +0200

    removed the useless Injector setter

--- Battleship/src/main/java/de/htwg/battleship/controller/IMasterController.java
+++ Battleship/src/main/java/de/htwg/battleship/controller/IMasterController.java
@@ -1,91 +1,127 @@
-//IMasterController.java
+// IMasterController.java
 package de.htwg.battleship.controller;
 
-import com.google.inject.Injector;
+import java.util.Map;
+import java.util.Set;
+
 import de.htwg.battleship.model.IPlayer;
 import de.htwg.battleship.model.IShip;
 import de.htwg.battleship.observer.IObservable;
+import de.htwg.battleship.util.GameMode;
 import de.htwg.battleship.util.State;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * IMasterController is an Utility-Interface.
  * it uses intern several Controller
+ * 
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-16
  */
-public interface IMasterController extends IObservable {
+public interface IMasterController extends
+                IObservable {
 
     /**
      * Method to shoot on a board.
      *
-     * @param x x-Coordinate where to shoot.
-     * @param y y-Coordinate where to shoot.
-     * checks the winner automatically after each shot
+     * @param x
+     *            x-Coordinate where to shoot.
+     * @param y
+     *            y-Coordinate where to shoot.
+     *            checks the winner automatically after each shot
      */
-    void shoot(int x, int y);
+         void shoot(int x, int y);
 
     /**
      * Method to place a Ship on the board.
-     * @param x x-Coordinate for the start point
-     * @param y y-Coordinate for the start point
-     * @param orientation of the ship, true for horizontal, false for vertical
+     * 
+     * @param x
+     *            x-Coordinate for the start point
+     * @param y
+     *            y-Coordinate for the start point
+     * @param orientation
+     *            of the ship, true for horizontal, false for vertical
      */
-    void placeShip(int x, int y, boolean orientation);
+         void placeShip(int x, int y, boolean orientation);
 
     /**
      * To get the current State of the game.
      * uses the different states in the util Package
+     * 
      * @return current state
      */
-    State getCurrentState();
+          State getCurrentState();
 
     /**
      * Setter for a specified Viewer to change the presentation.
-     * @param state new Viewer
+     * 
+     * @param state
+     *            new Viewer
      */
-    void setCurrentState(State state);
+         void setCurrentState(State state);
 
     /**
      * Getter for the first Player.
+     * 
      * @return first Player
      */
-    IPlayer getPlayer1();
+            IPlayer getPlayer1();
 
     /**
      * Getter for the second Player.
+     * 
      * @return second Player
      */
-    IPlayer getPlayer2();
+            IPlayer getPlayer2();
 
     /**
      * Method to set the players name.
      * Which players name is set is addicted to the current state of the game
-     * @param name name of the Player
+     * 
+     * @param name
+     *            name of the Player
      */
-    void setPlayerName(String name);
+         void setPlayerName(String name);
 
     /**
      * Method to start a game or if the game is in the END-state to
      * restart the game.
      */
-    void startGame();
-
-    /**
-     * Setter for the injector.
-     * @param injector
-     */
-    void setInjector(Injector injector);
+         void startGame();
 
     /**
      * Method to fill a Map with ship coordinates.
-     * @param shipList specified ships
-     * @param map Map where to save the ships
-     * @param ships how much ships are in the list
+     * 
+     * @param shipList
+     *            specified ships
+     * @param map
+     *            Map where to save the ships
+     * @param ships
+     *            how much ships are in the list
      * @return the new Map
      */
-    Map<Integer, Set<Integer>> fillMap(IShip[] shipList,
-        Map<Integer, Set<Integer>> map, int ships);
+       Map<Integer, Set<Integer>>
+          fillMap(IShip[] shipList, Map<Integer, Set<Integer>> map, int ships);
+
+    /**
+     * Method to get in the Options state, where you can set the GameMode.
+     * The Standard GameMode is the
+     * {@link de.htwg.battleship.util.GameMode#NORMAL}
+     */
+         void configureGame();
+
+    /**
+     * Setter for the GameMode.
+     * 
+     * @param gm
+     *            the new GameMode
+     */
+         void setGameMode(GameMode gm);
+
+    /**
+     * Getter for the GameMode.
+     * 
+     * @return the current GameMode
+     */
+             GameMode getGameMode();
 }
