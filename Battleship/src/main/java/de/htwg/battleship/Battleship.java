4ec6d1c0068d60238ea2a9a382cd48242e124e4c
Moritz Sauter <SauterMoritz@gmx.de>
2015-10-23 23:29:51 +0200

    feature(web): implemented singleton for the instance of the game and getter for all objects that can be useful to an external ui

--- Battleship/src/main/java/de/htwg/battleship/Battleship.java
+++ Battleship/src/main/java/de/htwg/battleship/Battleship.java
@@ -1,19 +1,18 @@
 // Battleship.java
 package de.htwg.battleship;
 
-import java.util.Scanner;
-
-import org.apache.log4j.PropertyConfigurator;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-
 import de.htwg.battleship.aview.gui.GUI;
 import de.htwg.battleship.aview.tui.TUI;
+import de.htwg.battleship.controller.IMasterController;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.util.Scanner;
 
 /**
  * Battleship start file.
- * 
+ *
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-04
@@ -21,33 +20,107 @@ import de.htwg.battleship.aview.tui.TUI;
 public final class Battleship {
 
     /**
-     * Private Constructor.
+     * The location of the log4j-property-file.
      */
-    private Battleship() {}
-
+    private static final String LOG4J_PROPERTY_CONFIG = "log4j.properties";
+    /**
+     * Saves the injector.
+     */
+    private static Injector injector;
+    /**
+     * Saves the master controller of all UIs.
+     */
+    private static IMasterController masterController;
     /**
      * Saves TUI.
      */
-    private static TUI     tui;
+    private static TUI tui;
+    /**
+     * Saves GUI.
+     */
+    private static GUI gui;
     /**
      * Scanner to read from stdin.
      */
     private static Scanner scanner;
+    /**
+     * Saves a instance of this game. Is used in a Singleton-Pattern.
+     */
+    private static Battleship instance;
+
+    /**
+     * Private Constructor.
+     */
+    private Battleship() {
+        PropertyConfigurator.configure(LOG4J_PROPERTY_CONFIG);
+        injector = Guice.createInjector(new BattleshipModule());
+        masterController = injector.getInstance(IMasterController.class);
+        tui = injector.getInstance(TUI.class);
+        gui = injector.getInstance(GUI.class);
+    }
+
+    /**
+     * Singleton to get an instance of this game.
+     *
+     * @return the same instance of battleship
+     */
+    public static Battleship getInstance() {
+        if (instance == null) {
+            instance = new Battleship();
+        }
+        return instance;
+    }
 
     /**
      * Main-Method.
-     * 
-     * @param args
-     *            not used
+     *
+     * @param args not used
      */
     public static void main(final String[] args) {
-        PropertyConfigurator.configure("log4j.properties");
-        Injector injector = Guice.createInjector(new BattleshipModule());
-        tui = injector.getInstance(TUI.class);
-        injector.getInstance(GUI.class);
+        @SuppressWarnings("unused") Battleship bs = Battleship.getInstance();
         scanner = new Scanner(System.in);
-        while (true) {
-            tui.processInputLine(scanner.nextLine());
+        boolean done = false;
+        while (!done) {
+            done = tui.processInputLine(scanner.nextLine());
         }
+        // exit because TUI detected 'exit' as input string
+        // exit that the GUI closes too
+        System.exit(0);
+    }
+
+    /**
+     * Getter for TUI.
+     *
+     * @return the TUI of the current instance
+     */
+    public TUI getTui() {
+        return tui;
+    }
+
+    /**
+     * Getter for GUI.
+     *
+     * @return the GUI of the current instance
+     */
+    public GUI getGui() {
+        return gui;
+    }
+
+    /**
+     * Getter for the guice injector.
+     *
+     * @return the injector of the current instance
+     */
+    public Injector getInjector() {
+        return injector;
+    }
+
+    /**
+     * Getter for the master controller.
+     *
+     * @return the controller for the current instance
+     */
+    public IMasterController getMasterController() {
+        return masterController;
     }
 }
