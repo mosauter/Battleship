36954cc8d427d60edb590b3ad57934aad2fef4b4
Moritz Sauter <SauterMoritz@gmx.de>
2016-03-22 15:20:35 +0100

    refactor(module): exchanged decorater with productive controller

--- Battleship/src/main/java/de/htwg/battleship/BattleshipModule.java
+++ Battleship/src/main/java/de/htwg/battleship/BattleshipModule.java
@@ -4,9 +4,8 @@ package de.htwg.battleship;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
-
 import de.htwg.battleship.controller.IMasterController;
-import de.htwg.battleship.controller.impl.MasterDecoratorController;
+import de.htwg.battleship.controller.impl.MasterController;
 import de.htwg.battleship.model.IBoard;
 import de.htwg.battleship.model.IPlayer;
 import de.htwg.battleship.model.IShip;
@@ -15,9 +14,9 @@ import de.htwg.battleship.model.impl.Player;
 import de.htwg.battleship.model.impl.Ship;
 
 /**
- * BattleshipModule provides a configuration of the game with
- * Dependency Injection managed with guice.
- * 
+ * BattleshipModule provides a configuration of the game with Dependency
+ * Injection managed with guice.
+ *
  * @author Moritz Sauter (SauterMoritz@gmx.de)
  * @version 1.00
  * @since 2014-12-19
@@ -26,7 +25,7 @@ public class BattleshipModule extends AbstractModule {
 
     @Override
     protected final void configure() {
-        bind(IMasterController.class).to(MasterDecoratorController.class)
+        bind(IMasterController.class).to(MasterController.class)
                                      .in(Singleton.class);
         bind(IBoard.class).to(BoardField.class);
         bind(IPlayer.class).to(Player.class);
