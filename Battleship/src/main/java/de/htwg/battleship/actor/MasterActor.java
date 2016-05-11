2ee77b811c84540d4d557d2ec50e859d4715cf82
Moritz Sauter <SauterMoritz@gmx.de>
2016-05-11 21:02:16 +0200

    feature(master actor): added the implemented actors to the master actor

--- Battleship/src/main/java/de/htwg/battleship/actor/MasterActor.java
+++ Battleship/src/main/java/de/htwg/battleship/actor/MasterActor.java
@@ -5,7 +5,9 @@ package de.htwg.battleship.actor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import de.htwg.battleship.actor.childactor.ShipActor;
 import de.htwg.battleship.actor.childactor.ShootActor;
+import de.htwg.battleship.actor.messages.ShipMessage;
 import de.htwg.battleship.actor.messages.ShootMessage;
 
 /**
@@ -16,12 +18,17 @@ import de.htwg.battleship.actor.messages.ShootMessage;
  */
 public class MasterActor extends UntypedActor {
 
-    ActorRef shootActors = getContext().actorOf(Props.create(ShootActor.class), "Shooters");
+    private ActorRef shootActors =
+        getContext().actorOf(Props.create(ShootActor.class), "Shooters");
+    private ActorRef shipActor =
+        getContext().actorOf(Props.create(ShipActor.class), "Shipper");
 
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof ShootMessage) {
             shootActors.forward(message, getContext());
+        } else if (message instanceof ShipMessage) {
+            shipActor.forward(message, getContext());
         }
         unhandled(message);
     }
