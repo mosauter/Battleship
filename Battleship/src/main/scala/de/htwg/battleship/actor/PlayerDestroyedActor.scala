420231d83f50bc770aebf872ae6d6ac575d3d407
Moritz Sauter <SauterMoritz@gmx.de>
2016-06-17 22:26:47 +0200

    wip(destroyer player): fixed the sender bug

--- Battleship/src/main/scala/de/htwg/battleship/actor/PlayerDestroyedActor.scala
+++ Battleship/src/main/scala/de/htwg/battleship/actor/PlayerDestroyedActor.scala
@@ -1,12 +1,13 @@
 package de.htwg.battleship.actor
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorRef, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import de.htwg.battleship.actor.messages.{PlayerDestroyedMessage, PlayerDestroyedResponse, ShipDestroyedMessage}
 import de.htwg.battleship.model.{IPlayer, IShip}
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
@@ -24,30 +25,31 @@ class PlayerDestroyedActor extends Actor {
     val shipper = context.actorOf(Props[ShipDestroyedActor], ShipDestroyedActor.ACTOR_NAME)
     implicit val timeout = Timeout(5 seconds)
 
-    import context.dispatcher
-
+    // TODO: create an abstract or a helper object
     def reduce(one: Boolean, two: Boolean): Boolean = one && two
 
     def createFutures(shipList: Array[IShip], player: IPlayer): List[Future[Boolean]] = {
         var list = new ListBuffer[Future[Boolean]]()
+        // TODO: think of replacement with loop until board.getShips
         for (ship <- shipList) {
             list += (shipper ? ShipDestroyedMessage(ship, player)).mapTo[Boolean]
         }
         list.toList
     }
 
-    def handleMessage(msg: PlayerDestroyedMessage) = {
+    def handleMessage(msg: PlayerDestroyedMessage, ref: ActorRef) = {
         val futureList = createFutures(msg.player.getOwnBoard.getShipList, msg.player)
         val aggrFuture = Future sequence futureList
         aggrFuture onSuccess {
             case results: List[Boolean] =>
                 val result = results.reduce(reduce)
-                sender() ! PlayerDestroyedResponse(result, msg.first)
+                ref ! PlayerDestroyedResponse(result, msg.first)
         }
     }
 
     override def receive: Receive = {
-        case msg: PlayerDestroyedMessage => handleMessage(msg)
+        case msg: PlayerDestroyedMessage =>
+            handleMessage(msg, sender())
         case msg => unhandled(msg)
     }
 }
